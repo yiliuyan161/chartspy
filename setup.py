385c900dc789d75265f74f59f273263fd5a9a2d7
yiliuyan <chenjiajia1@gmail.com>
2021-08-07 14:49:23 +0800

    版本更新到v1.1.5

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_install_requires():
 
 setup(
     name="echartspy",
-    version="1.1.3",
+    version="1.1.5",
     url="https://echartspy.icopy.site/",
     description="echarts python wrapper",
     keywords='echarts python ',
