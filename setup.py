219cd7e14d8311eb7e87f3409dd6931b9e1fbcc1
chenjiajia <chenjiajia1@gmail.com>
2021-09-02 13:59:59 +0800

    v1.3.4 bug修复，scatter_echarts函数

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_install_requires():
 
 setup(
     name="chartspy",
-    version="1.3.3",
+    version="1.3.4",
     url="https://chartspy.icopy.site/",
     description="echarts&g2plot python wrapper",
     keywords='echarts g2plot python ',
