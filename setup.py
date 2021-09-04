f6f13624e12821ba110e8e99949df4deea27da77
chenjiajia <chenjiajia1@gmail.com>
2021-09-04 21:38:53 +0800

    v1.3.6

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_install_requires():
 
 setup(
     name="chartspy",
-    version="1.3.5",
+    version="1.3.6",
     url="https://chartspy.icopy.site/",
     description="echarts&g2plot python wrapper",
     keywords='echarts g2plot python ',
