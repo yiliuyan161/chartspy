a3bfde69ed62f9f5ccc4de3b9b4b5e5fed49e772
chenjiajia <chenjiajia@licaimofang.com>
2021-12-29 14:05:40 +0800

    bug fix

--- chartspy/express/__init__.py
+++ chartspy/express/__init__.py
@@ -1,4 +1,6 @@
-import echarts
-import g2plot
+from chartspy.express.echarts import *
+from chartspy.express.g2plot import *
+import chartspy.express.echarts as ecs
+import chartspy.express.g2plot as g2
 
-__all__ = echarts.__all__.extend(g2plot.__all__)
+__all__ = ecs.__all__.extend(g2.__all__)
