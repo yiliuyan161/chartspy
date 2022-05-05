3c3473aeb30a7fa6f5c51a4fa1e1015f2358b5cc
yiliuyan <yiliuyan161@126.com>
2022-05-05 22:54:27 +0800

    Echarts 增加timeline 组装

--- chartspy/echarts.py
+++ chartspy/echarts.py
@@ -6,6 +6,7 @@ import re
 import uuid
 
 import simplejson
+from typing import List
 
 from .base import Tools, GLOBAL_ENV, Html, json_type_convert, FUNCTION_BOUNDARY_MARK
 
@@ -223,6 +224,100 @@ class Echarts(object):
         self.extra_js = extra_js
         self.with_gl = with_gl
 
+    @staticmethod
+    def timeline(echarts_dict: dict = {}, visual_map_options=None):
+        """
+
+        :param echarts_dict: key类型字符串，用作标题, value类型 Echarts对象，或者Echarts对象的options
+        :param visual_map_options: visualMap 配置,visualMap最大值最小值需要配置的时候设置,格式参照echarts官方文档
+        :return:
+        """
+        charts_dict = {k: (v.options if isinstance(v, Echarts) else v) for k, v in echarts_dict.items()}
+        options = {
+            "baseOption": {
+                'timeline': {
+                    'axisType': 'category',
+                    'orient': 'vertical',
+                    'autoPlay': True,
+                    'inverse': True,
+                    'playInterval': 1000,
+                    'left': None,
+                    'right': 0,
+                    'top': 20,
+                    'bottom': 20,
+                    'width': 55,
+                    'height': None,
+                    'symbol': 'none',
+                    'checkpointStyle': {
+                        'borderWidth': 2
+                    },
+                    'controlStyle': {
+                        'showNextBtn': False,
+                        'showPrevBtn': False
+                    },
+                    'data': [],
+                },
+                'grid': {
+                    'top': 100,
+                    'containLabel': True,
+                    'left': 30,
+                    'right': '110'
+                },
+                'animationDurationUpdate': 1000,
+                'animationEasingUpdate': 'quinticInOut'
+            },
+            'options': []
+        }
+        keys = list(charts_dict.keys())
+        keys.sort()
+        options['baseOption']['timeline']['data'] = keys
+        if len(keys) > 0:
+            if 'tooltip' in charts_dict[keys[0]].keys():
+                options['baseOption']['tooltip'] = charts_dict[keys[0]]['tooltip']
+            if 'xAxis' in charts_dict[keys[0]].keys():
+                options['baseOption']['xAxis'] = charts_dict[keys[0]]['xAxis']
+            if 'yAxis' in charts_dict[keys[0]].keys():
+                options['baseOption']['yAxis'] = charts_dict[keys[0]]['yAxis']
+
+            if visual_map_options is not None:
+                options['baseOption']['visualMap'] = visual_map_options
+            elif 'visualMap' in charts_dict[keys[0]].keys():
+                options['baseOption']['visualMap'] = charts_dict[keys[0]]['visualMap']
+            if 'series' in charts_dict[keys[0]].keys():
+                options['baseOption']['series'] = charts_dict[keys[0]]['series']
+            for i in range(0, len(keys)):
+                options['options'].append({
+                    'title': {
+                        'show': True,
+                        'text': keys[i]
+                    },
+                    'series': charts_dict[keys[i]]['series']
+                })
+                if 'visualMap' in charts_dict[keys[i]].keys() and 'visualMap' in options['baseOption'].keys():
+                    if isinstance(charts_dict[keys[i]]['visualMap'], list) and isinstance(
+                            options['baseOption']['visualMap'], list) and len(charts_dict[keys[i]]['visualMap']) == len(options['baseOption']['visualMap']):
+                        for j in range(0, len(options['baseOption']['visualMap'])):
+                            if 'min' in options['baseOption']['visualMap'][j].keys():
+                                options['baseOption']['visualMap'][j]['min'] = min(
+                                    options['baseOption']['visualMap'][j]['min'],
+                                    charts_dict[keys[i]]['visualMap'][j]['min'])
+                            if 'max' in options['baseOption']['visualMap'][j].keys():
+                                options['baseOption']['visualMap'][j]['max'] = min(
+                                    options['baseOption']['visualMap'][j]['max'],
+                                    charts_dict[keys[i]]['visualMap'][j]['max'])
+                    elif isinstance(charts_dict[keys[i]]['visualMap'], dict) and isinstance(
+                            options['baseOption']['visualMap'], dict):
+                        if 'min' in options['baseOption']['visualMap'].keys():
+                            options['baseOption']['visualMap']['min'] = min(
+                                options['baseOption']['visualMap']['min'],
+                                charts_dict[keys[i]]['visualMap']['min'])
+                        if 'max' in options['baseOption']['visualMap'].keys():
+                            options['baseOption']['visualMap']['max'] = max(
+                                options['baseOption']['visualMap']['max'],
+                                charts_dict[keys[i]]['visualMap']['max'])
+
+        return Echarts(options=options)
+
     def overlap_series(self, other_chart_options: list = []):
         """
         叠加其他配置中的Series数据到现有配置，现有配置有多个坐标轴的，建议Series声明对应的axisIndex
@@ -304,7 +399,6 @@ class Echarts(object):
             html_file.write(html)
         abs_path = os.path.abspath(path)
         return Html("<p>{path}</p>".format(path=abs_path))
-   
 
     def render_html(self) -> str:
         """
