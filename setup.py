ba3af4112e6e384a6490039a74327d5cd2c0150f
yiliuyan <yiliuyan161@126.com>
2024-08-28 19:25:51 +0800

    升级版本

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def get_install_requires():
 
 setup(
     name="chartspy",
-    version="2.1.9",
+    version="2.2.1",
     url="https://chartspy.icopy.site/",
     description="echarts g2plot klinechart highcharts tabulator python wrapper",
     keywords='echarts g2plot klinechart highcharts tabulator python ',
